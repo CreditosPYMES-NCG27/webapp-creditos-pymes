7d9f1558d60d411b37140d3a3ae07f36f678ed9d
EsneiderCdD <ecd.97@hotmail.com>
2025-10-05 13:53:52 -0500

    feat: Button jsx y css

--- src/features/HeroSection/HeroSection.jsx
+++ src/features/HeroSection/HeroSection.jsx
@@ -1,14 +1,16 @@
 import './HeroSection.css';
+
 export default function HeroSection() {
   return (
     <div className="container heroe text-center p-5 rounded my-5">
-      <h1 className="display-4">Webapp Créditos PYMES</h1>
-      <p className="lead">
-        Sistema de gestión de créditos para pequeñas y medianas empresas
-      </p>
-      <button className="btn btn-primary btn-lg mt-3">
-        Comenzar
-      </button>
+      <div className="content">
+        <h1 className="display-4">
+          Soluciones financieras adaptadas a cada etapa de crecimiento
+        </h1>
+        <button className="btn btn-primary btn-lg mt-3">
+          Comenzar
+        </button>
+      </div>
     </div>
   );
 }
