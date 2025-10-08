c39d3f756df0d48354cf8b23442c9aa1184f3189
EsneiderCdD <ecd.97@hotmail.com>
2025-10-07 22:03:16 -0500

    feat : validators

--- src/components/Button/ButtonActions.js
+++ src/components/Button/ButtonActions.js
@@ -4,19 +4,14 @@ export const ButtonActions = {
   loginPartner: (navigate) => navigate("/login/partner"),
   register: (navigate) => navigate("/register"),
   alert: () => alert("Ejemplo"),
-
-  login: () => {
-    const email = document.getElementById("email")?.value;
-    const password = document.getElementById("password")?.value;
-
+  login: (navigate, email, password) => {
     if (!email || !password) {
       alert("Por favor complete usuario y contraseña ❌");
       return;
     }
-
     if (email === "admin@test.com" && password === "1234") {
       alert("✅ Login correcto");
-
+      navigate("/");
     } else {
       alert("Credenciales incorrectas ❌");
     }
