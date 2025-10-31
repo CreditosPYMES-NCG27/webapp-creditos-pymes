1ca14ca36361fb1764002025418edb00ffb4f91d
Fabian Rodriguez <fabianrodriguez264@hotmail.com>
2025-10-30 21:28:21 -0500

    Se agregan modificaciones al registro de usuario

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,14 +2,72 @@ import { useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 import "../features/SignIn/SignIn.css";
+import { supabase } from "../auth/supabaseClient";
 
 const SignInPage = () => {
     const [showSecondForm, setShowSecondForm] = useState(false);
+    const [formData, setFormData] = useState({
+        nombreEmpresa: "",
+        identificadorFiscal: "",
+        telefonoEmpresa: "",
+        direccionEmpresa: "",
+        direccionEmpresa2: "",
+        nombre: "",
+        apellido: "",
+        email: "",
+        password: "",
+        confirmarPassword: "",
+    });
+    const handleChange = (e) => {
+    setFormData({...formData,[e.target.name]: e.target.value,});
+    };
 
     const handleSiguiente = (e) => {
         e.preventDefault();
         setShowSecondForm(true);
     };
+
+    const handleSubmit = async (e) => {
+    e.preventDefault();
+
+        if (formData.password !== formData.confirmarPassword) {
+        alert("Las contraseñas no coinciden");
+        return;
+        }
+
+        try {
+        const { data, error } = await supabase.auth.signUp({
+            email: formData.email,
+            password: formData.password,
+            options: {
+            data: {
+                nombre: formData.nombre,
+                apellido: formData.apellido,
+                empresa: formData.nombreEmpresa,
+                telefono_empresa: formData.telefonoEmpresa,
+                direccion_empresa: formData.direccionEmpresa,
+                direccion_empresa2: formData.direccionEmpresa2,
+                identificador_fiscal: formData.identificadorFiscal,
+            },
+            },
+        });
+
+        if (error) {
+            console.error(error);
+            alert("Error al registrar: " + error.message);
+            return;
+        }
+
+      alert("Usuario registrado exitosamente.");
+      console.log("Usuario registrado:", data);
+
+        } catch (err) {
+        console.error("Error inesperado:", err);
+        alert("Ocurrió un error al registrar el usuario.");
+        }
+  };
+
+
     return(
         <div>
             <Navbar />
@@ -34,52 +92,52 @@ const SignInPage = () => {
                     </div>
                     {/*form */}
                     <div className="w-80 border rounded mt-3" style={{ minWidth: "340px" }}>
-                       <form className="p-4 border-2">
+                       <form onSubmit={!showSecondForm ? handleSiguiente : handleSubmit} className="p-4 border-2">
                         {!showSecondForm ? (
                             <>
                                 <div class="mb-3">
                                     <label class="form-label">Nombre de la Empresa</label>
-                                    <input type="text" class="form-control" id="" required/>
+                                    <input type="text" name="nombreEmpresa" value={formData.nombreEmpresa} onChange={handleChange} className="form-control" required/>
                                 </div>
                                 <div class="mb-3">
                                     <label class="form-label">Identificador Fiscal de la Empresa</label>
-                                    <input type="text" class="form-control" id="" required/>
+                                    <input type="text" name="identificadorFiscal" value={formData.identificadorFiscal} onChange={handleChange} className="form-control" required/>
                                 </div>
                                 <div class="mb-3 ms-0">
                                     <label class="form-label">Teléfono de la Empresa</label>
-                                    <input type="text" class="form-control" id="" required/>
+                                    <input type="text" name="telefonoEmpresa" value={formData.telefonoEmpresa} onChange={handleChange} className="form-control" required/>
                                 </div>
                                 <div class="mb-3 ms-0">
                                     <label class="form-label">Dirección de la Empresa</label>
-                                    <input type="text" class="form-control" id="" required/>
+                                    <input type="text" name="direccionEmpresa" value={formData.direccionEmpresa} onChange={handleChange} className="form-control" required/>
                                 </div>
                                 <div class="mb-3">
                                     <label class="form-label">Dirección de la Empresa 2(Opcional)</label>
-                                    <input type="text" class="form-control" id=""/>
+                                    <input type="text" name="direccionEmpresa2" value={formData.direccionEmpresa2} onChange={handleChange} className="form-control" id=""/>
                                 </div>
-                                <button type="submit" class="btn color-btn w-100" onClick={handleSiguiente}>Siguiente</button>
+                                <button type="submit" className="btn color-btn w-100" onClick={handleSiguiente}>Siguiente</button>
                             </>
                         ) : (
                             <>
                                 <div className="mb-3">
                                     <label className="form-label">Nombre</label>
-                                    <input type="text" className="form-control" required />
+                                    <input type="text" name="nombre" value={formData.nombre} onChange={handleChange} className="form-control" required />
                                 </div>
                                 <div className="mb-3">
                                     <label className="form-label">Apellido</label>
-                                    <input type="text" className="form-control" required />
+                                    <input type="text" name="apellido" value={formData.apellido} onChange={handleChange} className="form-control" required />
                                 </div>
                                 <div className="mb-3 ms-0">
                                     <label className="form-label">Email</label>
-                                    <input type="email" className="form-control" required />
+                                    <input type="email" name="email" value={formData.email} onChange={handleChange} className="form-control" required />
                                 </div>
                                 <div className="mb-3 ms-0">
                                     <label className="form-label">Contraseña</label>
-                                    <input type="password" className="form-control" required />
+                                    <input type="password" name="password" value={formData.password} onChange={handleChange} className="form-control" required />
                                 </div>
                                 <div className="mb-3">
                                     <label className="form-label">Confirmar Contraseña</label>
-                                    <input type="password" className="form-control" required />
+                                    <input type="password" name="confirmarPassword" value={formData.confirmarPassword} onChange={handleChange} className="form-control" required />
                                 </div>
                                 <div className="form-check mb-3">
                                 <input className="form-check-input" type="checkbox" value="" id="checkDefault" required/>
